2ab03e6a1310c1aa94d245f23c14e4c562de8271
alejis86 <83306945+alejis86@users.noreply.github.com>
2021-05-22 10:31:53 -0700

    Add files via upload

---
+++ precache-manifest.c5926ad7e805e5a20082bce8e79de120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "411d6ab275bc8976c295",
+    "url": "/mayra-contract-2/static/css/main.58e43969.chunk.css"
+  },
+  {
+    "revision": "411d6ab275bc8976c295",
+    "url": "/mayra-contract-2/static/js/main.411d6ab2.chunk.js"
+  },
+  {
+    "revision": "3267dc31f788255d6bc6",
+    "url": "/mayra-contract-2/static/css/1.1288f4dd.chunk.css"
+  },
+  {
+    "revision": "3267dc31f788255d6bc6",
+    "url": "/mayra-contract-2/static/js/1.3267dc31.chunk.js"
+  },
+  {
+    "revision": "4aa265a823176fa86396",
+    "url": "/mayra-contract-2/static/js/runtime~main.4aa265a8.js"
+  },
+  {
+    "revision": "144c2e18bb0e6a91e330d25d3214b57c",
+    "url": "/mayra-contract-2/index.html"
+  }
+];
